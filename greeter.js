709efb8aaddd04c885ef9ced77410b2b36b74e37
Nathan Costea <nathanC@knowclassic.com>
2016-09-20 19:15:43 -0400

    Wrote a copy of the entire code from memory: interface, class, function, DOM.

--- greeter.js
+++ greeter.js
@@ -13,4 +13,18 @@ function greeter(person) {
 var user = new Student("Mary", "M.", "Jane");
 //var user = [0, 1, 2];
 document.body.innerHTML = greeter(user);
+var Student1 = (function () {
+    function Student1(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = this.firstName + " " + this.middleInitial + " " + this.lastName;
+    }
+    return Student1;
+}());
+function greet(person) {
+    return "Hello" + " " + person.firstName + " " + person.lastName;
+}
+var user = new Student1("Nathan", "S.", "Perdan");
+document.body.innerHTML = greet(user);
 //# sourceMappingURL=greeter.js.map
--- greeter.ts
+++ greeter.ts
@@ -18,3 +18,24 @@ var user = new Student("Mary", "M.", "Jane");
 //var user = [0, 1, 2];
 
 document.body.innerHTML = greeter(user);
+
+
+class Student1 {
+    fullName: string;
+    constructor (public firstName: string, public middleInitial: string, public lastName: string){
+        this.fullName = this.firstName + " " + this.middleInitial + " " + this.lastName;
+    }
+}
+
+interface Person1 {
+    firstName: string;
+    lastName: string;
+}
+
+function greet(person: Person1) {
+    return "Hello" + " " + person.firstName + " " + person.lastName;
+}
+
+var user = new Student1("Nathan", "S.", "Perdan");
+
+document.body.innerHTML = greet(user);
